a6eb19dad1fa7a8ac2e23066bb88c4a0ccd92575
zurbash <duta1101@bk.ru>
2022-04-06 14:49:49 +0400

    added new logic, now books are displayed while looping through object array

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
+//modal inputs
 let author = document.querySelector(".author")
 let title = document.querySelector(".title")
 let pages = document.querySelector(".pages")
 let read = document.querySelector(".read")
 let submit = document.querySelector(".submit")
+let closeModal = document.querySelector(".closeModal")
+//main content
+let body = document.querySelector("body")
 let container = document.querySelector(".container")
 let add = document.querySelector(".addBtn")
 let modal = document.querySelector(".modal")
-let closeModal = document.querySelector(".closeModal")
 let myLibrary = [];
 
+//shows modal
 add.addEventListener('click', () => {modal.style.display='block';})
+//closes modal
 closeModal.addEventListener('click', () => {modal.style.display='none'; clear();})
-
+//constructor
 function book(author, title, pages, read) {
     this.author=author
     this.title=title
     this.pages=pages
     this.read=read
 }
-  
-  function addBookToLibrary(author,title,pages,read) {
-    let obj = new book(author,title,pages,read);
-    myLibrary.push(obj);
+function createBook(){
+    let tempAuthor = author.value;
+    let tempTitle = title.value;
+    let tempPages = pages.value;
+    let tepRead = read.value;
+    let newBook = new book(tempAuthor, tempTitle, tempPages, tepRead);
+    addBookToLibrary(newBook);
 }
-addBookToLibrary.prototype = Object.create(book);
-
-function displayBook(){
-    let obj = myLibrary[myLibrary.length-1];
+//adds book objects in array
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+function createCard(i){
+    let obj = myLibrary[i];
     let card = document.createElement("div");
-    card.setAttribute('id',myLibrary.length-1);
     card.classList.add("card")
+    card.setAttribute('id',i);
     let title = document.createElement("p");
     let author = document.createElement("p");
     let pages = document.createElement("p");
@@ -39,16 +49,13 @@ function displayBook(){
     title.textContent=obj.title;
     author.textContent=obj.author;
     pages.textContent=obj.pages;
-    if(obj.read=="on"){readStatus.textContent="Read"}
-    else{readStatus.textContent="Not Read"}
     removeButton.textContent="Remove";
-    createCard(card,title,author,pages,readStatus,removeButton);
-    addRemoveButton(removeButton,card);
+    toggleReadButton(obj,readStatus);
+    appendChild(card,title,author,pages,readStatus,removeButton);
+    addRemoveButton(removeButton,card,i);
     addReadStatus(readStatus,card);
-
-
 }
-function createCard(card,title,author,pages,readStatus,removeButton){
+function appendChild(card,title,author,pages,readStatus,removeButton){
     container.appendChild(card)
     card.appendChild(title)
     card.appendChild(author)
@@ -56,15 +63,9 @@ function createCard(card,title,author,pages,readStatus,removeButton){
     card.appendChild(readStatus)
     card.appendChild(removeButton)
 }
-function addRemoveButton(removeButton,card){
-    removeButton.setAttribute('id', myLibrary.length-1);
-    removeButton.classList.add("removeButton")
-    removeButton.addEventListener('click', () => {removeBook(card.getAttribute('id'));})
-}
-function removeBook(id){
-    let removeCard=document.getElementById(id);
-    container.removeChild(removeCard);
-    myLibrary.splice(id,1)
+function toggleReadButton(obj,readStatus){
+    if(obj.read=="on"){readStatus.textContent="Read"}
+    else{readStatus.textContent="Not Read"}
 }
 function addReadStatus(readStatus,card){
     if  (readStatus.textContent=="Read"){readStatus.style.backgroundColor="rgba(0, 128, 0, 0.555)"}
@@ -74,12 +75,41 @@ function addReadStatus(readStatus,card){
         else{readStatus.style.backgroundColor="rgba(0, 128, 0, 0.555)";readStatus.textContent="Read"}
      })
 }
+function addRemoveButton(removeButton,card,i){
+    removeButton.setAttribute('id', i);
+    removeButton.classList.add("removeButton")
+    removeButton.addEventListener('click', () => {removeBook(card.getAttribute('id'));})
+}
+//removes book objects from array and displays remaining books
+function removeBook(id){
+    myLibrary.splice(id,1);
+    displayBook();
+}
+//displays books on screen
+function displayBook(){
+    empty(container);
+    body.appendChild(container)
+    for(let i=0;i<myLibrary.length;i++){
+        createCard(i);
+    }
+}
+//removes cards from dom
+function empty(element) {
+    while(element.firstElementChild) {
+       element.firstElementChild.remove();
+    }
+  }
+//clears input in modal
 function clear(){
     author.value="";
     title.value="";
     pages.value="";
     read.checked=false;
 }
-let book1 = new book("joe", "coolbook", 122,  "read");
+function validate(){
+    if(author.value ==="" || title.value === "" || pages.value === ""){return false}
+    else{return true}
+}
 
-submit.addEventListener('click', () => {addBookToLibrary(author.value,title.value,pages.value,read.value); displayBook();  clear(); });
+//submit book info
+submit.addEventListener('click', () =>  {if(validate()==true){createBook();  displayBook(); clear();  modal.style.display='none';}});
